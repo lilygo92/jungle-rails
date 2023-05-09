0dcf81ed1db7b0064883f9fdcc8c7e0bf3e18dd9
Lily Go <lilygo7192@gmail.com>
2023-05-08 23:41:28 -0700

    Add cypress test for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe("Home Page", () => {
+
+  it ("visits the home page of the site", () => {
+      cy.visit('/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
